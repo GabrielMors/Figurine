a870a587f2f4ac92f60d12728877ad5be8ac5ccc
Gabriel Mors <gabrielmors210@gmail.com>
2023-07-29 14:14:21 -0400

    criando as abas de navegaçao, com a tabView

--- Figurine/ContentView.swift
+++ Figurine/ContentView.swift
@@ -9,70 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var itensNaColecao: Int = 0
-    
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Mingle")
-                .font(.title)
-                .bold()
-                .padding()
-            
-            HStack {
-                Text("Frase:")
-                    .bold()
-                Text("Problema em dobro")
-            }
-            .padding(.leading)
-            
-            HStack {
-                Image("figura_01")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 120)
-                
-                VStack(alignment: .leading) {
-                    PropriedadesView()
-                    PropriedadesView(imagem: "bolt", color: Color.yellow, nome: "Potência:", valor: "60%")
+        TabView {
+            FiguraDetalheView()
+                .tabItem {
+                    Label("Figuras", systemImage: "face.smiling")
                 }
-            }
-            .padding()
-            
-            HStack {
-                Spacer()
-                Text("O Mingle se destaca por fazer o dobro do trabalho na metade do tempo, com extrema precisão. Essas habilidades são úteis para ela em sua função de Analista de Dados Sênior para uma empresa internacional de computação em nuvem. Ela também tem uma propensão para dança de salão, dança de linha e praticamente qualquer tipo de atividade que a deixe dançar ao som da música.")
-                    .fontWeight(.light)
-                    .foregroundColor(Color.gray)
-                Spacer()
-            }
-            
-            Spacer()
-            
-            HStack() {
-                Spacer()
-                Button {
-                    itensNaColecao += 1
-                } label: {
-                    
-                    if itensNaColecao == 0 {
-                        Text("Adicionar á coleção")
-                            .padding()
-                            .background(.gray)
-                            .cornerRadius(30)
-                            .foregroundColor(.white)
-                    } else {
-                        Text("Na sua coleção: \(itensNaColecao)")
-                            .padding()
-                            .background(.green)
-                            .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
-                            .foregroundColor(.white)
-                    }
+            FiguraDetalheView()
+                .tabItem {
+                    Label("Figuras", systemImage: "face.dashed")
                 }
-                Spacer()
-                
-            }.padding()
-            
         }
+            
     }
 }
 
---
+++ Figurine/FiguraDetalheView.swift
@@ -0,0 +1,83 @@
+//
+//  FiguraDetalheView.swift
+//  Figurine
+//
+//  Created by Gabriel Mors  on 29/07/23.
+//
+
+import SwiftUI
+
+struct FiguraDetalheView: View {
+    
+    @State var itensNaColecao: Int = 0
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Mingle")
+                .font(.title)
+                .bold()
+                .padding()
+            
+            HStack {
+                Text("Frase:")
+                    .bold()
+                Text("Problema em dobro")
+            }
+            .padding(.leading)
+            
+            HStack {
+                Image("figura_01")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120)
+                
+                VStack(alignment: .leading) {
+                    PropriedadesView()
+                    PropriedadesView(imagem: "bolt", color: Color.yellow, nome: "Potência:", valor: "60%")
+                }
+            }
+            .padding()
+            
+            HStack {
+                Spacer()
+                Text("O Mingle se destaca por fazer o dobro do trabalho na metade do tempo, com extrema precisão. Essas habilidades são úteis para ela em sua função de Analista de Dados Sênior para uma empresa internacional de computação em nuvem. Ela também tem uma propensão para dança de salão, dança de linha e praticamente qualquer tipo de atividade que a deixe dançar ao som da música.")
+                    .fontWeight(.light)
+                    .foregroundColor(Color.gray)
+                Spacer()
+            }
+            
+            Spacer()
+            
+            HStack() {
+                Spacer()
+                Button {
+                    itensNaColecao += 1
+                } label: {
+                    
+                    if itensNaColecao == 0 {
+                        Text("Adicionar á coleção")
+                            .padding()
+                            .background(.gray)
+                            .cornerRadius(30)
+                            .foregroundColor(.white)
+                    } else {
+                        Text("Na sua coleção: \(itensNaColecao)")
+                            .padding()
+                            .background(.green)
+                            .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                    }
+                }
+                Spacer()
+                
+            }.padding()
+            
+        }
+    }
+}
+
+struct FiguraDetalheView_Previews: PreviewProvider {
+    static var previews: some View {
+        FiguraDetalheView()
+    }
+}
