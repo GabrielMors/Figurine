c8338f59769f1ced7172be9deb15d430ada7623b
Gabriel Mors <gabrielmors210@gmail.com>
2023-07-28 21:02:43 -0400

    colocando if e else. Alterando estados do botão

--- Figurine/ContentView.swift
+++ Figurine/ContentView.swift
@@ -61,6 +61,8 @@ struct ContentView: View {
             HStack {
                 Spacer()
                 Text("O Mingle se destaca por fazer o dobro do trabalho na metade do tempo, com extrema precisão. Essas habilidades são úteis para ela em sua função de Analista de Dados Sênior para uma empresa internacional de computação em nuvem. Ela também tem uma propensão para dança de salão, dança de linha e praticamente qualquer tipo de atividade que a deixe dançar ao som da música.")
+                    .fontWeight(.light)
+                    .foregroundColor(Color.gray)
                 Spacer()
             }
             
@@ -71,11 +73,21 @@ struct ContentView: View {
                 Button {
                     itensNaColecao += 1
                 } label: {
-                    Text("Na sua coleção: \(itensNaColecao)")
-                        .padding()
-                        .background(.green)
-                        .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
-                        .foregroundColor(.white)
+                    
+                    if itensNaColecao == 0 {
+                        Text("Adicionar á coleção")
+                            .padding()
+                            .background(.gray)
+                            .cornerRadius(30)
+                            .foregroundColor(.white)
+                    } else {
+                        Text("Na sua coleção: \(itensNaColecao)")
+                            .padding()
+                            .background(.green)
+                            .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                    }
+                    
                 }
                 Spacer()
             }.padding()
