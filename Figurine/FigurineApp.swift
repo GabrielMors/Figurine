3792d38381a3357ac5416bb691378d1d6bc2d038
Gabriel Mors <gabrielmors210@gmail.com>
2023-07-28 20:06:56 -0400

    agrupar objetos com staks e ajustando a interface

---
+++ Figurine/FigurineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigurineApp.swift
+//  Figurine
+//
+//  Created by Gabriel Mors  on 28/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FigurineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
