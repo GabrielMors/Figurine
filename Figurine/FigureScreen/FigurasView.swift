8f81952aba0c322409ccfcd6d2e0ed607acff511
Gabriel Mors <gabrielmors210@gmail.com>
2023-07-31 06:36:56 -0400

    criado uma lista para armazenar as figuras, e criado um file para a model

--- Figurine/FigureScreen/FigurasView.swift
+++ Figurine/FigureScreen/FigurasView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct FigurasView: View {
+    
+    var figuras = ["Migle", "Yodel"]
+    
+    @State var apresentado = false
+    
     var body: some View {
-        NavigationLink(destination: FiguraDetalheView()) {
-            Text("Mingle")
+        List(figuras, id: \.self) { figura in
+            Button {
+                apresentado = true
+            } label: {
+                Text(figura)
+            }
+            .sheet(isPresented: $apresentado) {
+                FiguraDetalheView()
+            }
         }
     }
 }
-
 struct FiguraView_Previews: PreviewProvider {
     static var previews: some View {
         FigurasView()
---
+++ Figurine/Model/ModeloDados.swift
@@ -0,0 +1,12 @@
+//
+//  ModeloDados.swift
+//  Figurine
+//
+//  Created by Gabriel Mors  on 31/07/23.
+//
+
+import Foundation
+
+struct Figura: Identifiable {
+    var id = UUID()
+}
